f89a01f01f6e7c5cf24ee56e3cfe5488955f468f
Sebastian Meckovski <sebastian.meckovski@ef.com>
2024-10-14 22:28:24 +0200

    script refactored

--- POC/main.py
+++ POC/main.py
@@ -3,11 +3,11 @@ import pandas as pd
 # Read data from text files with specified dtypes
 admin1_df = pd.read_csv("admin1_codes.txt", sep=" ", header=None, names=["code", "admin1_name"],
     dtype={"code": int, "admin1_name": str}, )
-alternate_names_df = pd.read_csv("alternate_names.txt", sep="\t", header=None, names=["geoname_id", "lang", "name"],
-    dtype={"geoname_id": int, "lang": str, "name": str}, )
+alternate_names_df = pd.read_csv("alternate_names.txt", sep="\t", header=None, names=["geonameId", "isoLanguage", "name"],
+    dtype={"geonameId": int, "isoLanguage": str, "name": str}, )
 cities_df = pd.read_csv("cities.txt", sep=" ", header=None,
-    names=["city_geoname_id", "city_name", "code", "country_code"],
-    dtype={"city_geoname_id": int, "city_name": str, "code": int, "country_code": int}, )
+    names=["geonameId", "city_name", "code", "country_code"],
+    dtype={"geonameId": int, "city_name": str, "code": int, "country_code": int}, )
 
 # Create a DataFrame for country codes (replace with your actual data if you have a country_codes.txt file)
 country_codes_df = pd.DataFrame(
@@ -20,27 +20,27 @@ def join_and_localize(language_code):
     and country names based on the given language code using merge operations.
     """
 
-    # Merge cities_df with alternate_names_df on city_geoname_id
-    merged_df = pd.merge(cities_df, alternate_names_df[alternate_names_df["lang"] == language_code],
-        left_on="city_geoname_id", right_on="geoname_id", how="left", )
+    # Merge cities_df with alternate_names_df on geonameId
+    merged_df = pd.merge(cities_df, alternate_names_df[alternate_names_df["isoLanguage"] == language_code],
+        left_on="geonameId", right_on="geonameId", how="left", )
 
     # Fill missing city names with original values
     merged_df["city_name"] = merged_df["name"].fillna(merged_df["city_name"])
 
     # Merge with admin1_df and fill missing admin names
     merged_df = pd.merge(merged_df, admin1_df, on="code", how="left")
-    merged_df = pd.merge(merged_df, alternate_names_df[alternate_names_df["lang"] == language_code], left_on="code",
-        right_on="geoname_id", how="left", )
+    merged_df = pd.merge(merged_df, alternate_names_df[alternate_names_df["isoLanguage"] == language_code], left_on="code",
+        right_on="geonameId", how="left", )
     merged_df["admin1_name"] = merged_df["name_y"].fillna(merged_df["admin1_name"])
 
     # Merge with country_codes_df and fill missing country names
     merged_df = pd.merge(merged_df, country_codes_df, on="country_code", how="left")
-    merged_df = pd.merge(merged_df, alternate_names_df[alternate_names_df["lang"] == language_code],
-        left_on="country_code", right_on="geoname_id", how="left", )
+    merged_df = pd.merge(merged_df, alternate_names_df[alternate_names_df["isoLanguage"] == language_code],
+        left_on="country_code", right_on="geonameId", how="left", )
     merged_df["country_name"] = merged_df["name"].fillna(merged_df["country_name"])
 
     # Select and rename columns
-    result_df = merged_df[["city_geoname_id", "city_name", "admin1_name", "country_name"]]
+    result_df = merged_df[["geonameId", "city_name", "admin1_name", "country_name"]]
     result_df = result_df.rename(columns={"admin1_name": "admin_area", "country_name": "country"})
 
     return result_df
