a07546247ab53467813505c145fbfa677e6d674f
sebastian-meckovski <sebastian.meckovski@gmail.com>
2024-10-24 23:17:34 +0200

    todo notes added

---
+++ JupyterScript/todo.py
@@ -0,0 +1,6 @@
+# TODO:
+# 1. Find out what python 'in' really means
+# 2. Optimize cirles function to run based on geohashes
+# 3. Separate script functions
+# 4. Format code to look more readable
+# 5. Implement code into the pipeline
