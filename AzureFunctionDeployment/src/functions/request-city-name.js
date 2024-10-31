521c9db0206093b8086d73a22934c43211116bb9
sebastian-meckovski <sebastian.meckovski@gmail.com>
2024-10-31 19:47:44 +0100

    test key retrieval from key vault

--- AzureFunctionDeployment/src/functions/request-city-name.js
+++ AzureFunctionDeployment/src/functions/request-city-name.js
@@ -1,4 +1,11 @@
 const { app } = require('@azure/functions');
+const { DefaultAzureCredential } = require('@azure/identity');
+const { SecretClient } = require('@azure/keyvault-secrets');
+
+const keyVaultName = "MongoDB-credentials";
+const keyVaultUrl = `https://${keyVaultName}.vault.azure.net`;
+const credential = new DefaultAzureCredential();
+const client = new SecretClient(keyVaultUrl, credential);
 
 app.http('request-city-name', {
     methods: ['GET'],
@@ -7,6 +14,15 @@ app.http('request-city-name', {
         context.log(`Http function processed request for url "${request.url}"`);
         const name = request.query.get('name') || await request.text() || 'world';
 
-        return { body: `Hello, ${name}! DEPLOYED GEO DATA GENERATOR REPO. TESTING KEY DEPLOY` };
+        try {
+            const secretName = "geo-names-mongo-connection-string";
+            const secret = await client.getSecret(secretName);
+            const connectionString = secret.value;
+
+            return { body: `Retrieved connection string: ${connectionString}` };
+        } catch (error) {
+            context.log.error('Error retrieving secret from Key Vault', error);
+            return { body: 'Error retrieving secret from Key Vault' };
+        }
     }
 });
