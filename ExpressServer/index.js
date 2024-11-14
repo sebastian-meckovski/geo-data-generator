d0af6afbccc7edbc32a1c08f77b37bb73571ae08
sebastian-meckovski <sebastian.meckovski@gmail.com>
2024-11-14 17:28:57 +0100

    cors added

--- ExpressServer/index.js
+++ ExpressServer/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const cityNameByCoordinates = require('./routes/cityInfoByCoordinates');
 const cityNameByAscii = require('./routes/cityInfoByAscii');
 
 const app = express();
 const port = 5050;
 
+app.use(cors());
+
 app.use('/city-info-by-coordinates', cityNameByCoordinates);
-app.use('/city-info-by-ascii', cityNameByAscii)
+app.use('/city-info-by-ascii', cityNameByAscii);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
