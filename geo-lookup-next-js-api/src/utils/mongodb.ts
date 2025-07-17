b4cecf964e6e95f4a9139395619540c5ee40e457
Sebastian Meckovski <sebastian.meckovski@ef.com>
2025-07-17 11:54:20 +0200

    Refactor MongoDB connection logic by removing development mode handling and simplifying error message formatting.

--- geo-lookup-next-js-api/src/utils/mongodb.ts
+++ geo-lookup-next-js-api/src/utils/mongodb.ts
@@ -1,32 +1,17 @@
-// utils/mongodb.ts
-
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 const connString = process.env.MONGO_DB_CONN_STRING;
 
 if (!connString) {
-  throw new Error('Please define the MONGO_DB_CONN_STRING environment variable');
+  throw new Error(
+    "Please define the MONGO_DB_CONN_STRING environment variable"
+  );
 }
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable to preserve the value
-  // across module reloads caused by HMR (Hot Module Replacement).
-  let globalWithMongo = global as typeof globalThis & {
-    _mongoClientPromise?: Promise<MongoClient>;
-  };
-
-  if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(connString);
-    globalWithMongo._mongoClientPromise = client.connect();
-  }
-  clientPromise = globalWithMongo._mongoClientPromise!
-} else {
-  // In production mode, it's best to not use a global variable.
-  client = new MongoClient(connString);
-  clientPromise = client.connect();
-}
+client = new MongoClient(connString);
+clientPromise = client.connect();
 
 export default clientPromise;
